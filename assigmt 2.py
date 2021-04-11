3d67f1d726013da8df469a0659a38636c73d91f5
irenekinyanjui <81644723+irenekinyanjui@users.noreply.github.com>
2021-04-11 10:44:17 -0500

    Add files via upload

---
+++ assigmt 2.py
@@ -0,0 +1,15 @@
+first_name = "irene"
+last_name = "Kinya"
+print ("name" ": " + first_name, last_name)
+country = "Kenya"
+print("Country" ":" + country)
+profession= "Coach"
+print ("Profession"  ":" + profession)
+email = "njeri@yahoo.com"
+print("email" + ":" +email)
+phone = ("549384499")
+print("phone"+ ":",phone)
+city = "Houston"
+print("City"+ ":",city)
+address = "32845 Sweet flower lane"
+print("Address" "+" ,address)
